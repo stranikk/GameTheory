4247719d3b811c98733bd8718c5d4571dcc53325
Vulerols <nikk178@mail.ru>
2020-04-29 01:14:19 +0300

    Update lr3.py
    fix bug nesh - pareto algs

--- LR3/lr3.py
+++ LR3/lr3.py
@@ -1,136 +1,147 @@
 import numpy as np
+from numpy.linalg import inv, det
 import pandas as pd
-import os
 import random
 
-def calc_nesh(matrix_input):
-    result = []
-    for i in range(0,len(matrix_input)):
-        max_1 = -51
-        max_2 = -51
-        max_1_j = 0
-        max_2_j = 0
-        for j in range(0,len(matrix_input[i])):
-            val = matrix_input[j][i]
-            if val[0] > max_1:
-                max_1 = val[0]
-                max_1_j = j
+class Game:
+    __game = []
+    __nesh = []
+    __pareto = []
 
-        for i in range(0,len(matrix_input[max_1_j])):
-            val = matrix_input[max_1_j][i]
-            if val[1] > max_2:
-                max_2 = val[1]
-                max_2_j = i
+    def add_game(self, input_game):
+        self.__game = input_game
     
-        if max_1 == matrix_input[max_1_j][max_2_j][0]:
-            result.append([max_1,max_2])
+    def get_game(self):
+        return self.__game
+
+    def get_nesh(self):
+        return self.__nesh
     
-    if len(result) != 0:
-        return result
-    else:
-        print("Nesh not found\n")
+    def get_pareto(self):
+        return self.__pareto
 
-def calc_pareto(matrix_input):
-    cheak_list = []
-    cheak_list_max = []
-    cheak_list_max_iter = []
-    result = []
-    new_result = []
-    for i in range(0,len(matrix_input)):
-        for j in range(0,len(matrix_input[i])):
-            val = matrix_input[i][j]
-            for k in range(0,len(matrix_input)):
-                for m in range(0,len(matrix_input[k])):
-                    val_iter = matrix_input[k][m]
-                    if (val_iter[0] >= val[0]) and (val_iter[1] >= val[1]):
-                        val = val_iter
+    def optimal_strat(self, row, col):
+        item = self.__game[row, col]
+        for i in range(self.__game.shape[0]):
+            for j in range(self.__game.shape[1]):
+                current = self.__game[i, j]
+                if (item < current).any() and (item <= current).all():
+                    return False
+        return True
 
-            if (val[0] == matrix_input[i][j][0]) and (val[1] == matrix_input[i][j][1]):
-                cheak_list.append(val)
-    
-    max_1 = -51
-    max_2 = -51
+    def pareto_optimal(self):
+        optimal_strategies = []
+        for i in range(self.__game.shape[0]):
+            for j in range(self.__game.shape[1]):
+                if self.optimal_strat(i, j):
+                    optimal_strategies.append((i, j))
+                
+        self.__pareto = optimal_strategies
 
-    for i in range(0,len(matrix_input)):
-        for j in range(0,len(matrix_input[i])):
-            val = matrix_input[i][j]
-            if val[0] > max_1:
-                max_1 = val[0]
-            if val[1] > max_2:
-                max_2 = val[1]
+    def count_maximum(self,lst):
+        indexes = np.linspace(0, lst.shape[0] - 1, lst.shape[0], dtype = int)   
+        return indexes[np.isin(lst, np.max(lst))]
 
-    for i in range(0,len(matrix_input)):
-        for j in range(0,len(matrix_input[i])):
-            val = matrix_input[i][j]
-            if (max_1 == val[0]) or (max_2 == val[1]):
-                cheak_list_max_iter.append(val)
-    
-    for vl in cheak_list_max_iter:
-        for vl1 in cheak_list_max_iter:
-            if vl[0] >= vl1[0] and vl[1] >= vl1[1]:
-                cheak_list_max.append(vl)
+    def maximum_1(self, i):
+        indexes = self.count_maximum(self.__game[:, i, 0])
+        return indexes[self.__game[indexes, i, 0] >= np.max(self.__game[indexes, i, 0])]
 
-    for val in cheak_list_max:
-        result.append(val)
-    for val1 in cheak_list:
-        result.append(val) 
+    def maximum_2(self, i):
+        indexes = self.count_maximum(self.__game[i, :, 1])
+        return indexes[self.__game[indexes, i, 1] >= np.max(self.__game[indexes, i, 1])]
 
-    return result
+    def nash_optimal(self):
+        optimal_strategies = []
+        for i in range(self.__game.shape[0]):
+            second_ids = self.maximum_2(i)
+            for j in second_ids:
+                first_ids = self.maximum_1(j)
+                if i in first_ids:
+                    optimal_strategies.append((i, j))
+        self.__nesh = optimal_strategies
 
+    def format_game(self):
+        formatted_array = [["(%s | %s)" % (str(j[0]), str(j[1])) for j in i] for i in self.__game]
+        df = pd.DataFrame(formatted_array)
+        return df
+
+def print_game(game):
+    main_game = Game()
+    main_game.add_game(game)
+    main_game.nash_optimal()
+    main_game.pareto_optimal()
+    game = main_game.get_game()
+
+    nesh = []
+    pareto = []
+    for val in main_game.get_nesh():
+        x = val[0]
+        y = val[1]
+        nesh.append(game[x][y])
+
+    for val in main_game.get_pareto():
+        x = val[0]
+        y = val[1]
+        pareto.append(game[x][y])
+
+    print("Game: ", main_game.format_game())
+    print("Nash: ", nesh)
+    print("Pareto: ", pareto)
 
 if __name__ == "__main__":
-    N = 10
-    matrix = []
-    steps_broun_rob = 3000
-    for j in range(0,N):
-        iter_arr = []
-        for i in range(0,N):
-            iter_val = []
-            iter_val.append(random.randint(-50, 50))
-            iter_val.append(random.randint(-50, 50))
-            iter_arr.append(iter_val)
-        matrix.append(iter_arr)
+    print("____________________ПРОВЕРКА АЛГОРИТМА____________________")
+    
+    print("Перекресток со смещением:")
+    epsilon = round(random.random(), 3)
+    z = round(random.random(), 3)
+    crosswayBiMatrix = np.array(
+        [[[1, 1], [1 - epsilon, 2]],
+        [[2, 1 - z], [0, 0]]])
+    print_game(crosswayBiMatrix)
 
-    matrix_challenge_robber = [[[-5,-5],[0,-10]],[[-10,0],[-1,-1]]]
-    print("Challenge_robber: ",matrix_challenge_robber)
-    print("Nesh: ",calc_nesh(matrix_challenge_robber))
-    print("Pareto: ",calc_pareto(matrix_challenge_robber))
+    print("Семейный спор:")
+    familyDisputeBiMatrix = np.array(
+        [[[4, 1], [0, 0]],
+        [[0, 0], [1, 4]]]
+    )
+    print_game(familyDisputeBiMatrix)
 
-    matrix_family_challenge = [[[4,1],[0,0]],[[0,0],[1,4]]]
-    print("Challenge_family: ",matrix_family_challenge)
-    print("Nesh: ",calc_nesh(matrix_family_challenge))
-    print("Pareto: ",calc_pareto(matrix_family_challenge))
+    print("Дилемма заключенного:")
+    prisonerDilemmaBiMatrix = np.array(
+        [[[ -5, -5], [ 0, -10]],
+        [[-10,  0], [-1,  -1]]]
+    )
+    print_game(prisonerDilemmaBiMatrix)
 
-    matrix_family_crossroads = [[[1,1],[1,2]],[[2,1],[0,0]]]
-    print("Challenge_crossroads: ",matrix_family_crossroads)
-    print("Nesh: ",calc_nesh(matrix_family_crossroads))
-    print("Pareto: ",calc_pareto(matrix_family_crossroads))
+    print("____________________РЕШЕНИЯ НА МАТРИЦАХ____________________")
+    print("Случайная матрица 10х10:")
+    game = np.random.randint(-50, 50, (10, 10, 2))
+    print_game(game)
+    
+    print("Матрица варианта №13:")
+    BiMatrixVariant = np.array([
+        [[ 4, 1], [6, 2]],
+        [[11, 7], [0, 5]]
+    ])
+    print_game(BiMatrixVariant)
 
-    print(matrix)
-    print("Nesh: ",calc_nesh(matrix))
-    print("Pareto: ",calc_pareto(matrix))
+    A = BiMatrixVariant[:,:,0]
+    B = BiMatrixVariant[:,:,1]
+    u = np.ones(2)
 
-    matrix_variant = [[[4,1],[6,2]],[[11,7],[0,5]]]
+    v1 = 1/(u.dot(inv(A)).dot(u))
+    v2 = 1/(u.dot(inv(B)).dot(u))
 
-    print("Nesh: ",calc_nesh(matrix_variant))
+    x = v2 * u.dot(inv(B))
+    y = v1 * inv(A).dot(u)
 
-    A = np.array([[4,6],[11,0]])
-    B = np.array([[1,2],[7,5]])
+    print('A:')
+    print(pd.DataFrame(A))
+    print('B:')
+    print(pd.DataFrame(B))
+    print()
+    print('v1:', round(v1, 3))
+    print('v2:', round(v2, 3))
 
-    u = np.array([[1,1]])
-    ut = u.transpose()
-    Bt = np.linalg.inv(B)
-    At = np.linalg.inv(A)
-    A_iter = np.dot(u,At)
-    B_iter = np.dot(u,Bt)
-    v1 = np.divide(1,np.dot(A_iter,ut))
-    v2 = np.divide(1,np.dot(B_iter,ut))
-    x = np.dot(np.dot(v2[0][0],u),Bt)
-    y = np.dot(np.dot(v1[0][0],At),ut)
-    print("x = ",x)
-    print("y = ",y)
-    print("v1 = ",round(v1[0][0],3))
-    print("v2 = ",round(v2[0][0],3))
-
-
-
+    print('x:', np.round(x, 3))
+    print('y:', np.round(y, 3))
